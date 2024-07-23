d2550394393b7e7a7ef66cbc668f3172175bd9f3
BrainCellCoder <70462882+BrainCellCoder@users.noreply.github.com>
2024-07-23 09:57:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your code here
+const pics = document.querySelectorAll("img");
+const selectedX = []
+const selectedDog = []
+pics.forEach((pic) =>{
+	pic.addEventListener("click", () =>{
+		if(pic.classList[1] == "selected"){
+			if(pic.id == "x"){
+				selectedX.pop();
+			}
+			if(pic.id == "dog"){
+				selectedDog.pop();
+			}
+			pic.classList.remove("selected");
+		}else{
+			pic.classList.add("selected");
+			if(pic.id == "x"){
+				selectedX.push(pic.id)
+			}else if(pic.id == "dog"){
+				selectedDog.push(pic.id)
+			}
+		}
+	})
+})
