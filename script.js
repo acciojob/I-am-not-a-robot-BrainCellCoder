40a7448fc0db0b755da5ecfc76b60fa978c655f7
BrainCellCoder <70462882+BrainCellCoder@users.noreply.github.com>
2024-07-23 10:38:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //your code here
-const pics = document.querySelectorAll("img");
+const pics = document.querySelectorAll("img")
+const msg = document.querySelector("p")
 const selectedX = []
 const selectedDog = []
 pics.forEach((pic) =>{
@@ -20,5 +21,41 @@ pics.forEach((pic) =>{
 				selectedDog.push(pic.id)
 			}
 		}
+	const selectedDogLength = selectedDog.length;
+	const selectedXLength = selectedX.length;
+		const totalLength = selectedDog.length + selectedX.length;
+	const resetBtn = document.querySelector(".reset")
+	const verifyBtn = document.querySelector(".verify")
+	if(selectedDogLength ==1 || selectedXLength==1 ){
+		resetBtn.removeAttribute('id');
+	}if(selectedDogLength >1 || selectedXLength>1){
+		verifyBtn.removeAttribute('id');
+	}if(totalLength == 1){
+		verifyBtn.id="btn"
+	}if(totalLength == 0){
+		resetBtn.id = "btn"
+	}
+	});
+})
+
+const resetBtn = document.querySelector(".reset")
+const verifyBtn = document.querySelector(".verify")
+
+resetBtn.addEventListener("click", () =>{
+	selectedDog.length=0
+	selectedX.length=0
+	pics.forEach((pic) =>{
+		pic.classList.remove("selected");
 	})
+	resetBtn.id="btn"
+	verifyBtn.id="btn"
+	msg.innerText=""
+})
+
+verifyBtn.addEventListener("click", () =>{
+	if(selectedX.length == 0 && selectedDog.length == 2){
+		msg.innerText = "You are a human. Congratulations!"
+	}else{
+		msg.innerText = "We can't verify you as a human. You selected the non-identical tiles."
+	}
 })
